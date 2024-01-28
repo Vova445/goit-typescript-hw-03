972a809ac8814f2d3d5dd146c307149920190084
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-01-28 21:35:46 +0200

    result

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-const key = new Key();
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person came in.');
+      } else {
+        console.log('The door is closed.');
+      }
+    }
+  }
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is opened.');
+      } else {
+        console.log('Invalid key. The door closed.');
+      }
+    }
+  }
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
+  export {};
+  
